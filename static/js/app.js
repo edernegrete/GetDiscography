63aa0d906d2e95637da8cad146de4558014d7eb3
Ederyairr <ederyairnglz@gmail.com>
2015-03-15 17:14:22 -0600

    New design, adds image of the artist

--- static/js/app.js
+++ static/js/app.js
@@ -1,7 +1,5 @@
 $(function(){
-
 	var submitBtn = $('#submit-btn');
-	
 	function submit () {
 		var artistName = $('#artistName').val();
 		var urlArtistName = "http://itunes.apple.com/search?term=" + artistName +"&entity=album&callback=?"
@@ -10,49 +8,60 @@ $(function(){
 			alert('Inserta un artista')
 			return false
 		}
-		console.log(urlArtistImage)
-		$.getJSON(urlArtistImage).done(function(data){
-			var image = data.responseData.results[2].url
-			var imageData = {
-				image : image
-			}
-			this.templateImage = Handlebars.compile($('#artist-image').html());
-			var templateImageData = this.templateImage(imageData)
-			$('#Artist').append(templateImageData)
- 		});
+			$.getJSON(urlArtistImage).done(function(data){
+				$('#Artist').removeClass('is-animated')
+				var image = data.responseData.results[2].url
+				var imageData = {
+					image : image,
+					artistName: artistName
+				}
+				this.templateImage = Handlebars.compile($('#artist-image').html());
+				var templateImageData = this.templateImage(imageData)
+				$('#Artist').html(templateImageData)
+				albums(urlArtistName, artistName);
+	 		});
+		}
+	function albums(urlArtistName, artistName){
 
 		$.getJSON(urlArtistName).done(function(data){
-				$('#obtainer').addClass('is-animated')
-				var numberOfAlbums = data.results.length
-				var numberOfAlbumdata = {
-					artistName: artistName,
-					numberOfAlbums: numberOfAlbums
-				}
-				this.numberOfAlbumTemplate = Handlebars.compile($('#numberOf-Albums').html())
-				var templateNumberData = this.numberOfAlbumTemplate(numberOfAlbumdata)
-				$('#Albums').append(templateNumberData)
+			$('#obtainer').addClass('is-animated')
+			var numberOfAlbums = data.results.length
+			var numberOfAlbumdata = {
+				artistName: artistName,
+				numberOfAlbums: numberOfAlbums
+			}
+			this.numberOfAlbumTemplate = Handlebars.compile($('#numberOf-Albums').html())
+			var templateNumberData = this.numberOfAlbumTemplate(numberOfAlbumdata)
+			$('#Albums').append(templateNumberData)
 
-				for (var i = 0; i<numberOfAlbums; i++){
-					var Albumsdata ={
-						Albumname: data.results[i].collectionName,
-						Artwork: data.results[i].artworkUrl100
-					}
-					
-					this.albumTemplate = Handlebars.compile($('#album-template').html())
-					var templateData = this.albumTemplate(Albumsdata);
-					$('#Albums').append(templateData)
+			for (var i = 0; i<numberOfAlbums; i++){
+				var Albumsdata ={
+					Albumname: data.results[i].collectionName,
+					Artwork: data.results[i].artworkUrl100
 				}
-				$('#returnBtn').on('click', returnToSearch)
+				this.albumTemplate = Handlebars.compile($('#album-template').html())
+				var templateData = this.albumTemplate(Albumsdata);
+				$('#Albums').append(templateData)
+			}
+			$('#returnBtn').on('click', returnToSearch)
 		})
+
 	}
 	function returnToSearch () {
 		$('#obtainer').removeClass('is-animated')
+		$('#artistName').val('')
+		$('#Artist').html('')
 		$('#Albums').html('')
 	}
-	$(submitBtn).on('click', submit)
-	$('#artistName').keypress(function (e){
-		if(e.which == 13){
-			submit();
-		}
+
+	$(submitBtn).on('click', function(){
+		$('#Artist').addClass('is-animated');
 	})
+	$('#artistName').keypress(function (e){
+		if(e.which === 13){
+			$('#Artist').addClass('is-animated')
+			submit();
+		};
+		
+	});
 })
